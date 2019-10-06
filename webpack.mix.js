1f1e11484e076caa9bea307d90f7e939a6c0e29a
Michael Copanos <michaelcopanos@Michaels-MBP.attlocal.net>
2019-10-06 14:43:10 -0500

    nav bar

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,4 @@ const mix = require('laravel-mix');
 
 mix
     .js('assets/js/main.js', 'web/js/main.js')
-    .sass('assets/sass/app.scss', 'web/css')
+    .sass('assets/sass/app.scss', 'web/css/')
